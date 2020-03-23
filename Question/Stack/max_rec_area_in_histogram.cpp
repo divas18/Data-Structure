42d2f31fef76114c54e24952cbfdbb7d1dd6c130
martian <martian@localhost.localdomain>
2020-03-24 00:32:49 +0530

    Tree deleted

---
+++ Question/Stack/max_rec_area_in_histogram.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, i;
+        cin >> n;
+        long arr[n], area, maxArea = 0;
+        for(i = 0; i < n; i++)
+            cin >> arr[i];
+
+        stack<int> st;
+        i = 0;
+        while(i <= n)
+        {
+            if(st.empty() || i < n && arr[st.top()] <= arr[i])
+                st.push(i++);
+            else
+            {
+                    int top = st.top();
+                    st.pop();
+                    if(st.empty())
+                        area = i * arr[top];
+                    else
+                        area = (i - st.top() - 1) * arr[top];
+                    if(area > maxArea)
+                        maxArea = area;
+            }
+        }
+        cout << maxArea << "\n";
+    }
+}
